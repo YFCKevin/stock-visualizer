5e0048b1968aee6df86d04961c0870d7960785d3
yifan <yifanchen0914@gmail.com>
2025-07-03 17:44:40 +0800

    0703 YiFan [chore]: params typo

--- src/main/java/com/gurula/stockMate/symbol/SymbolRepository.java
+++ src/main/java/com/gurula/stockMate/symbol/SymbolRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface SymbolRepository extends MongoRepository<Symbol, String> {
-    Optional<Symbol> findBySymbol(String symbolId);
+    Optional<Symbol> findBySymbol(String symbolName);
 }
